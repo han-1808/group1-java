0b10fa2f43fdfa63da5754ab5d2442e7a73caa77
YenPhamNhu <119447095+YenPhamNhu@users.noreply.github.com>
2024-02-12 20:10:22 +0700

    Initial project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.example.bookstore {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.bookstore to javafx.fxml;
+    exports org.example.bookstore;
+}
