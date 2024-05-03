05aae70e293cb59b8de959abd6e25bc5e97e0b83
han-1808 <49582098+han-1808@users.noreply.github.com>
2024-05-04 03:24:53 +0700

    create UI of book

---
+++ src/Books.java
@@ -0,0 +1,55 @@
+public class Books {
+    private String title;
+    private String author;
+    private String category;
+    private String shelfID;
+    private int available;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setShelfID(String shelfID) {
+        this.shelfID = shelfID;
+    }
+
+    public void setAvailable(int available) {
+        this.available = available;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getShelfID() {
+        return shelfID;
+    }
+
+    public int getAvailable() {
+        return available;
+    }
+
+    public Books(String title, String author, String category, String shelfID, int available){
+        this.title=title;
+        this.author=author;
+        this.available=available;
+        this.shelfID=shelfID;
+        this.category=category;
+    }
+}
