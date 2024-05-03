05d3b2248f19e73fabcd27ae4341d3afa6a81cb5
DinziloZ <nhatdo666@gmail.com>
2024-05-03 19:57:02 +0700

    Create UI for Finance on application

---
+++ src/FinanceWindow.java
@@ -0,0 +1,12 @@
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import java.sql.*;
+import java.util.Calendar;
+import java.util.Date;
+public class FinanceWindow {
+    
+}
