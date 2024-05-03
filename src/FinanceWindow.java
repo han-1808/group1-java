055e6ad97fb38a793ce4a1ec53f618c31508f727
DinziloZ <nhatdo666@gmail.com>
2024-05-03 19:58:09 +0700

    Calculation of Finance for app

--- src/FinanceWindow.java
+++ src/FinanceWindow.java
@@ -51,4 +51,139 @@ public class FinanceWindow {
     static private TextField totalMember;
     static private Label totalLabelMember;
     static private Label rowSumMember;
+
+    static{
+        transactionType = new TableColumn<>("Transaction Type");
+        transactionType.setPrefWidth(230);
+        transactionType.setCellValueFactory(new PropertyValueFactory<>("transactionType"));
+
+        occasion = new TableColumn<>("Occasion");
+        occasion.setPrefWidth(230);
+        occasion.setCellValueFactory(new PropertyValueFactory<>("occasion"));
+
+        id = new TableColumn<>(" ID");
+        id.setPrefWidth(115);
+        id.setCellValueFactory(new PropertyValueFactory<>("id"));
+
+        name = new TableColumn<>("Name");
+        name.setPrefWidth(230);
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        time = new TableColumn<>("Time");
+        time.setPrefWidth(115);
+        time.setCellValueFactory(new PropertyValueFactory<>("time"));
+
+        date = new TableColumn<>("Date");
+        date.setPrefWidth(115);
+        date.setCellValueFactory(new PropertyValueFactory<>("date"));
+
+        amount = new TableColumn<>("Amount");
+        amount.setPrefWidth(115);
+        amount.setCellValueFactory(new PropertyValueFactory<>("amount"));
+
+        enteredBy = new TableColumn<>("Entered By");
+        enteredBy.setPrefWidth(115);
+        enteredBy.setCellValueFactory(new PropertyValueFactory<>("enteredBy"));
+
+        searchBox = new TextField();
+        searchBox.setPromptText("search by ID");
+        searchBox.setPrefSize(400, 40);
+        searchBox.setMinSize(400, 40);
+        searchBox.setMaxSize(400, 40);
+
+        searchBtn = new Button();
+        searchBtn.setMinSize(40, 40);
+        searchBtn.setPrefSize(40, 40);
+        searchBtn.setMaxSize(40, 40);
+        char[] searchSign= Character.toChars(128269);
+        searchBtn.setText(String.valueOf(searchSign));
+        searchBtn.getStyleClass().add("btn-menu");
+        searchBtn.setStyle(" -fx-font-size:16;");
+
+        refreshBtn = new Button();
+        refreshBtn.setMinSize(40, 40);
+        refreshBtn.setPrefSize(40, 40);
+        refreshBtn.setMaxSize(40, 40);
+        char[] refreshSign= Character.toChars(128472);
+        refreshBtn.setText(String.valueOf(refreshSign));
+        refreshBtn.getStyleClass().add("btn-menu");
+        refreshBtn.setStyle(" -fx-font-size:16;");
+
+        search = new HBox();
+        search.getChildren().addAll(searchBox, searchBtn, refreshBtn);
+        search.setAlignment(Pos.CENTER);
+
+        searchOption = new ComboBox<String>();
+        searchOption.getItems().addAll("Salary", "Bonus","Medical Fee","All");
+        searchOption.setValue("All");
+        searchOption.setMinSize(150, 40);
+        searchOption.setPrefSize(150, 40);
+        searchOption.setMaxSize(150, 40);
+
+        centerTop = new HBox();
+        dateBox= getTime();
+        centerTop.getChildren().addAll(search, searchOption, dateBox);
+        centerTop.setPrefSize(1150, 90);
+        centerTop.setMaxSize(1150, 90);
+        centerTop.setSpacing(10);
+        centerTop.setAlignment(Pos.CENTER_RIGHT);
+
+        lowestCenter=new HBox();
+
+        total=new TextField();
+        total.setStyle("-fx-background-color: #D3D3D3;" +
+                " -fx-font-weight:bold;");
+        total.setAlignment(Pos.CENTER_RIGHT);
+        total.setMinSize(230,25);
+        total.setPrefSize(230,25);
+        total.setMaxSize(230,25);
+
+        totalLabel =new Label("Total");
+        totalLabel.setStyle("-fx-font-style:italic;" +
+                " -fx-text-fill: #D3D3D3;" +
+                " -fx-font-size: 16;");
+
+        rowSum =new Label("[Row Selected: "+"]");
+        rowSum.setStyle("-fx-font-style:italic;" +
+                " -fx-text-fill: #D3D3D3;" +
+                " -fx-font-size: 16;");
+        lowestCenter.getChildren().addAll(rowSum,totalLabel, total);
+        lowestCenter.setAlignment(Pos.CENTER_RIGHT);
+        lowestCenter.setSpacing(10);
+
+        table=null;
+        conn=ConnectionClass.getInstance();
+        try {
+            table = new TableView<EmployeeFinanceTable>(conn.getEmployeeFinanceHistory(searchBox.getText(), searchOption.getValue(),
+                    ((ComboBox) dateBox.getChildren().get(2)).getValue()+"-"+
+                    ((ComboBox) dateBox.getChildren().get(1)).getValue()+"-"+
+                    ((ComboBox) dateBox.getChildren().get(0)).getValue(),
+                    ((ComboBox) dateBox.getChildren().get(6)).getValue()+"-"+
+                    ((ComboBox) dateBox.getChildren().get(5)).getValue()+"-"+
+                    ((ComboBox) dateBox.getChildren().get(4)).getValue()));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        searchBtn.setOnAction(event -> {
+            try {
+                table.getItems().clear();
+                table.getItems().addAll(conn.getEmployeeFinanceHistory(searchBox.getText(), searchOption.getValue(),
+                        ((ComboBox) dateBox.getChildren().get(2)).getValue()+"-"+
+                                ((ComboBox) dateBox.getChildren().get(1)).getValue()+"-"+
+                                ((ComboBox) dateBox.getChildren().get(0)).getValue(),
+                        ((ComboBox) dateBox.getChildren().get(6)).getValue()+"-"+
+                                ((ComboBox) dateBox.getChildren().get(5)).getValue()+"-"+
+                                ((ComboBox) dateBox.getChildren().get(4)).getValue()));
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            rowSum.setText("[Row Selected: "+table.getItems().size()+"]");
+            int sum=0;
+            for(int c=0; c<table.getItems().size();c++){
+                sum+=table.getItems().get(c).getAmount();
+            }
+            total.setText(""+sum);
+            total.setAlignment(Pos.CENTER_RIGHT);
+        });
 }
