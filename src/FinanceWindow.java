db73499dec068e5083b374fced1d829cdfd0be46
DinziloZ <nhatdo666@gmail.com>
2024-05-03 19:59:52 +0700

    Refresh Member for Finance Window

--- src/FinanceWindow.java
+++ src/FinanceWindow.java
@@ -371,4 +371,104 @@ public class FinanceWindow {
          total.setText(""+sum);
          total.setAlignment(Pos.CENTER_RIGHT);
     }
+    private static void refreshMember(){
+        searchBoxMember.clear();
+        searchOptionMember.setValue("All");
+        dateBoxMember=getTime();
+        centerTopMember.getChildren().clear();
+        centerTopMember.getChildren().addAll(searchMember, searchOptionMember, dateBoxMember);
+        try {
+            tableMember.getItems().clear();
+            tableMember.getItems().addAll(conn.getMemberFeeHistory(searchBoxMember.getText(),searchOptionMember.getValue(),
+                    ((ComboBox) dateBoxMember.getChildren().get(2)).getValue()+"-"+
+                            ((ComboBox) dateBoxMember.getChildren().get(1)).getValue()+"-"+
+                            ((ComboBox) dateBoxMember.getChildren().get(0)).getValue(),
+                    ((ComboBox) dateBoxMember.getChildren().get(6)).getValue()+"-"+
+                            ((ComboBox) dateBoxMember.getChildren().get(5)).getValue()+"-"+
+                            ((ComboBox) dateBoxMember.getChildren().get(4)).getValue()));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        rowSumMember.setText("[Row Selected: "+tableMember.getItems().size()+"]");
+        int sum=0;
+        for(int c=0; c<tableMember.getItems().size();c++){
+            sum+=tableMember.getItems().get(c).getAmount();
+        }
+        totalMember.setText(""+sum);
+        totalMember.setAlignment(Pos.CENTER_RIGHT);
+    }
+
+    public static  VBox getFinanceWindow() {
+        refreshEmployee();
+        return centerVBox;
+    }
+    public static  VBox getFinanceMemberWindow(){
+        refreshMember();
+        return centerVBoxMember;
+    }
+
+    private static HBox getTime(){
+
+        Label label = new Label("to");
+        label.setMinSize(20, 40);
+        label.setPrefSize(20, 40);
+        label.setMaxSize(20, 40);
+        label.getStyleClass().add("label-search");
+        label.setAlignment(Pos.CENTER);
+
+        ComboBox<String> day=new ComboBox<String>();
+        ComboBox<String> month=new ComboBox<String>();
+        ComboBox<String> year=new ComboBox<String>();
+
+        day.getItems().addAll("01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","Any");
+        month.getItems().addAll("01","02","03","04","05","06","07","08","09","10","11","12","Any");
+        year.getItems().addAll("1995","1996","1997","1998","1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021","2022","2023","2024","Any");
+        day.setPrefSize(80,40);
+        day.setMaxSize(80,40);
+        month.setPrefSize(80,40);
+        month.setMaxSize(80,40);
+        year.setPrefSize(80,40);
+        year.setMaxSize(80,40);
+        day.getStyleClass().add("combo-box-edited");
+        month.getStyleClass().add("combo-box-edited");
+        year.getStyleClass().add("combo-box-edited");
+        day.setValue("01");
+        month.setValue("01");
+        year.setValue("1995");
+
+        ComboBox<String> day2=new ComboBox<String>();
+        ComboBox<String> month2=new ComboBox<String>();
+        ComboBox<String> year2=new ComboBox<String>();
+
+        day2.getItems().addAll("01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","Any");
+        month2.getItems().addAll("01","02","03","04","05","06","07","08","09","10","11","12","Any");
+        year2.getItems().addAll("1995","1996","1997","1998","1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021","2022","2023","2024","Any");
+        day2.setPrefSize(80,40);
+        day2.setMaxSize(80,40);
+        month2.setPrefSize(80,40);
+        month2.setMaxSize(80,40);
+        year2.setPrefSize(80,40);
+        year2.setMaxSize(80,40);
+        day2.getStyleClass().add("combo-box-edited");
+        month2.getStyleClass().add("combo-box-edited");
+        year2.getStyleClass().add("combo-box-edited");
+
+        HBox dateBox=new HBox();
+        dateBox.setSpacing(2);
+        dateBox.setMinSize(480,40);
+        dateBox.setPrefSize(480,40);
+        dateBox.setMaxSize(480,40);
+        dateBox.setAlignment(Pos.CENTER_RIGHT);
+        dateBox.setStyle("-fx-border-color: linear-gradient(#7FFF00,#32CD32)");
+
+
+        Calendar cal = Calendar.getInstance();
+        day2.setValue(cal.get(Calendar.DAY_OF_MONTH)<10? ("0"+cal.get(Calendar.DAY_OF_MONTH)):""+cal.get(Calendar.DAY_OF_MONTH));
+        month2.setValue((cal.get(Calendar.MONTH)+1)<10? ("0"+(cal.get(Calendar.MONTH)+1)):""+(cal.get(Calendar.MONTH)+1));
+        year2.setValue(""+cal.get(Calendar.YEAR));
+
+        dateBox.getChildren().addAll(day,month,year,label,day2,month2,year2);
+      return dateBox;
+    }
 }
