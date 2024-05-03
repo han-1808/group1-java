ef14376e079cc411681b7804a30d97ff7903ed5e
DinziloZ <nhatdo666@gmail.com>
2024-05-03 20:42:01 +0700

    Import necessary library for Admin UI

---
+++ src/AdminWindow.java
@@ -0,0 +1,20 @@
+import javafx.animation.Transition;
+import javafx.animation.TranslateTransition;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.sql.SQLException;
+import java.util.Random;
+
+import java.util.concurrent.TimeUnit;
+public class AdminWindow {
+    
+}
