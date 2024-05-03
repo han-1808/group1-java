c90b66f50aba381e6ee7f179ff615b607a19deaf
DinziloZ <nhatdo666@gmail.com>
2024-05-03 17:58:58 +0700

    Finish launcher and connecting database

--- src/Launcher.java
+++ src/Launcher.java
@@ -76,3 +76,92 @@ public class Launcher extends Application {
 
         hb.getChildren().addAll(minimize,close);
         hb.setAlignment(Pos.CENTER_LEFT);
+        grid.getChildren().addAll(hb,welcome,admin, libraryStuff, member, dbBox);
+        GridPane.setConstraints(hb,4,0,2,1);
+        GridPane.setConstraints(welcome,2,5,3,1);
+        GridPane.setConstraints(admin, 3, 7);
+        GridPane.setConstraints(libraryStuff, 3, 8);
+        GridPane.setConstraints(member, 3, 9);
+        GridPane.setConstraints(dbBox,2,10,2,1);
+
+        grid.autosize();
+
+        sc.getStylesheets().add("style.css");
+        admin.setOnAction(event -> {
+            Admin a=new Admin();
+            a.start(arg0,sc,"Admin");});
+        libraryStuff.setOnAction(event -> {
+            Admin a=new Admin();
+            a.start(arg0,sc,"Employee");});
+        member.setOnAction(event -> {
+            Admin a=new Admin();
+            a.start(arg0,sc, "Member");});
+            dbBtn.setOnAction(event -> {
+                Stage alarm=new Stage();
+                Label dbinfo= new Label();
+                VBox vBox=new VBox();
+                Button apply=new Button("Apply");
+                Button cancel=new Button("Cancel");
+                Scene serverinfo=new Scene(vBox,430,230);
+                TextField url=new TextField();
+                TextField username= new TextField();
+                PasswordField password = new PasswordField();
+                PasswordField softPass= new PasswordField();
+                HBox btnBox=new HBox();
+    
+                dbinfo.setText("Database Information ");
+                dbinfo.setStyle(" -fx-font-size: 16");
+    
+                url.setPromptText("Database url (Default: jdbc:mysql://localhost:3306/library)");
+                url.setFocusTraversable(false);
+                url.setOnMouseClicked(event1 -> apply.setText("Apply"));
+                username.setPromptText("Database username (Default: root)");
+                username.setFocusTraversable(false);
+                username.setOnMouseClicked(event1 -> apply.setText("Apply"));
+                password.setPromptText("Database user password Default: ");
+                password.setFocusTraversable(false);
+                password.setOnMouseClicked(event1 -> apply.setText("Apply"));
+                softPass.setPromptText("Software password");
+                softPass.setFocusTraversable(false);
+                softPass.setOnMouseClicked(event1 -> apply.setText("Apply"));
+    
+                btnBox.setAlignment(Pos.CENTER);
+                btnBox.setSpacing(20);
+                btnBox.getChildren().addAll(cancel, apply);
+                vBox.setAlignment(Pos.CENTER);
+                vBox.setSpacing(5);
+    
+                vBox.getChildren().addAll(dbinfo,url,username,password,softPass,btnBox);
+    
+                serverinfo.getStylesheets().add("style.css");
+                alarm.setScene(serverinfo);
+                alarm.initStyle(StageStyle.UNDECORATED);
+                alarm.initModality(Modality.APPLICATION_MODAL);
+                cancel.setOnAction(event1 -> alarm.close());
+                apply.setOnAction(event1 ->{
+                    ConnectionClass conn=ConnectionClass.getInstance();
+                    if(conn.changeServer(url.getText(), username.getText(), password.getText(), softPass.getText())){
+                        apply.setText("Done");
+                        apply.setDisable(true);
+                        cancel.setText("Close");
+                    }else{
+                        apply.setText("Failed");
+                    }
+    
+                });
+                alarm.show();
+            });
+    
+            close.setOnAction(event -> arg0.close());
+            minimize.setOnAction(event -> arg0.setIconified(true));
+    
+            arg0.initStyle(StageStyle.UNDECORATED);
+            arg0.setScene(sc);
+            arg0.show();
+    
+    
+    
+        }
+    
+    }
+    
