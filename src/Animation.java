5643f2161a036d44b86f06b27150b5afa8babbd6
DinziloZ <nhatdo666@gmail.com>
2024-05-03 20:02:23 +0700

    Updating Animation for button in the application

--- src/Animation.java
+++ src/Animation.java
@@ -1,6 +1,7 @@
 import javafx.scene.control.Button;
 
 import java.util.concurrent.TimeUnit;
+
 public class Animation implements Runnable {
     public static Animation obj=null;
     private Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8;
@@ -30,5 +31,83 @@ public class Animation implements Runnable {
         this.btn7=btn4;
         this.btn8=btn5;
         running=true;
+    }
+
+    @Override
+    public void run() {
+
+        if (!running) {
+            if (!stretched1) {
+                stretched1 = true;
+                int i = 0;
+                while (i < 31) {
+                    btn1.setPrefSize(300, i);
+                    btn2.setPrefSize(300, i);
+                    btn3.setPrefSize(300, i);
+
+
+                    i++;
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(12);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+                stretched1 = false;
+                int i = 30;
+                while (i > -1) {
+                    btn1.setPrefSize(300, i);
+                    btn2.setPrefSize(300, i);
+                    btn3.setPrefSize(300, i);
+
+                    i--;
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(7);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }else{
+            if (!stretched2) {
+                stretched2 = true;
+                int i = 0;
+                while (i < 31) {
+                    btn4.setPrefSize(300, i);
+                    btn5.setPrefSize(300, i);
+                    btn6.setPrefSize(300, i);
+                    btn7.setPrefSize(300, i);
+                    btn8.setPrefSize(300, i);
+
+
+                    i++;
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(12);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+                stretched2 = false;
+                int i = 30;
+                while (i > -1) {
+                    btn4.setPrefSize(300, i);
+                    btn5.setPrefSize(300, i);
+                    btn6.setPrefSize(300, i);
+                    btn7.setPrefSize(300, i);
+                    btn8.setPrefSize(300, i);
+
+                    i--;
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(7);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }
     }
 }
