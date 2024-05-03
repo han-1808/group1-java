5fd012c9305f9eff219db010b26c13b9302d9332
DinziloZ <nhatdo666@gmail.com>
2024-05-03 20:26:10 +0700

    Creating Admin Function

---
+++ src/Admin.java
@@ -0,0 +1,19 @@
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.sql.SQLException;
+public class Admin {
+    
+}
